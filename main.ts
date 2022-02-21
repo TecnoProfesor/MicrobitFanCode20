d20a456437c4eab96fb61b2eb66c6b7a58834e4d
TecnoProfesor <65020790+TecnoProfesor@users.noreply.github.com>
2022-02-21 20:27:34 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,48 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        basic.showIcon(IconNames.Happy)
-        Avanza(40, 5000)
-        GiraIzq(60, 1)
-        Avanza(40, 5000)
-        GiraIzq(20, 1)
-        basic.showIcon(IconNames.Heart)
-    }
-    if (receivedNumber == 2) {
-        NeoPixel()
-        for (let index = 0; index < 30; index++) {
-            pins.digitalWritePin(DigitalPin.P8, 1)
-            pins.digitalWritePin(DigitalPin.P12, 1)
-            basic.pause(100)
-            pins.digitalWritePin(DigitalPin.P8, 0)
-            pins.digitalWritePin(DigitalPin.P12, 0)
-            basic.pause(100)
+    if (master == Math.trunc(receivedNumber / 10)) {
+        if (receivedNumber % 10 == 1) {
+            basic.showIcon(IconNames.Happy)
+            Avanza(40)
+            GiraIzq(40)
+            Avanza(40)
+            GiraIzq(40)
+            basic.showIcon(IconNames.Heart)
+        }
+        if (receivedNumber % 10 == 2) {
+            NeoPixel()
+            for (let index = 0; index < 30; index++) {
+                pins.digitalWritePin(DigitalPin.P8, 1)
+                pins.digitalWritePin(DigitalPin.P12, 1)
+                basic.pause(100)
+                pins.digitalWritePin(DigitalPin.P8, 0)
+                pins.digitalWritePin(DigitalPin.P12, 0)
+                basic.pause(100)
+            }
+        }
+        if (receivedNumber % 10 == 3) {
+            basic.showString("F A N T E C 2 0 2 2")
         }
     }
-    if (receivedNumber == 3) {
-        basic.showString("F A N T E C 2 0 2 2")
+    if (master == 1) {
+    	
     }
 })
-function Avanza (velocidad: number, tiempo: number) {
+function Avanza (velocidad: number) {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, velocidad)
-    deroscuro = 0
-    izqoscuro = 0
+    izqoscuro = 1
+    deroscuro = 1
+    while (izqoscuro == 1 && deroscuro == 1) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+            izqoscuro = 1
+        } else {
+            izqoscuro = 0
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            deroscuro = 1
+        } else {
+            deroscuro = 0
+        }
+    }
     while (izqoscuro == 0 || deroscuro == 0) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
             izqoscuro = 1
@@ -47,7 +64,7 @@ function Avanza (velocidad: number, tiempo: number) {
     maqueen.motorStop(maqueen.Motors.M1)
     maqueen.motorStop(maqueen.Motors.M2)
 }
-function GiraIzq (velocidad: number, tiempo: number) {
+function GiraIzq (velocidad: number) {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, velocidad)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velocidad)
     while (izqoscuro == 1 || deroscuro == 1) {
@@ -67,10 +84,10 @@ function GiraIzq (velocidad: number, tiempo: number) {
 }
 input.onSound(DetectedSound.Loud, function () {
     if (master == 1) {
-        radio.sendNumber(3)
+        radio.sendNumber(23)
         basic.pause(1000)
         basic.showString("F A N T E C 2 0 2 2")
-        radio.sendNumber(2)
+        radio.sendNumber(22)
         NeoPixel()
         for (let index = 0; index < 30; index++) {
             pins.digitalWritePin(DigitalPin.P8, 1)
@@ -80,16 +97,16 @@ input.onSound(DetectedSound.Loud, function () {
             pins.digitalWritePin(DigitalPin.P12, 0)
             basic.pause(100)
         }
-        radio.sendNumber(1)
+        radio.sendNumber(21)
         basic.showIcon(IconNames.Happy)
-        Avanza(40, 5000)
-        GiraIzq(60, 1)
-        Avanza(40, 5000)
-        GiraIzq(20, 1)
+        Avanza(40)
+        GiraIzq(40)
+        Avanza(40)
+        GiraIzq(40)
         basic.showIcon(IconNames.Heart)
     }
 })
-function GiroDer (velocidad: number, tiempo: number) {
+function GiroDer (velocidad: number) {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, velocidad)
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
     while (izqoscuro == 1 || deroscuro == 1) {
@@ -100,6 +117,43 @@ function GiroDer (velocidad: number, tiempo: number) {
         }
         if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             deroscuro = 1
+        } else {
+            deroscuro = 0
+        }
+    }
+    maqueen.motorStop(maqueen.Motors.M1)
+    maqueen.motorStop(maqueen.Motors.M2)
+}
+function Retrocede (velocidad: number) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, velocidad)
+    izqoscuro = 1
+    deroscuro = 1
+    while (izqoscuro == 1 && deroscuro == 1) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+            izqoscuro = 1
+        } else {
+            izqoscuro = 0
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            deroscuro = 1
+        } else {
+            deroscuro = 0
+        }
+    }
+    while (izqoscuro == 0 || deroscuro == 0) {
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+            izqoscuro = 1
+            maqueen.motorStop(maqueen.Motors.M1)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, velocidad)
+        } else {
+            izqoscuro = 0
+        }
+        if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            deroscuro = 1
+            maqueen.motorStop(maqueen.Motors.M2)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, velocidad)
         } else {
             deroscuro = 0
         }
@@ -137,10 +191,10 @@ function NeoPixel () {
 let BLUE = 0
 let GREEN = 0
 let RED = 0
-let izqoscuro = 0
 let deroscuro = 0
+let izqoscuro = 0
 let master = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
-master = 1
+master = 2
 radio.setGroup(1)
